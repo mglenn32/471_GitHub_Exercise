2a68297d2e79c470834ece4efb30ff9620e7f5e5
mglenn32 <mglenn32@liberty.edu>
2025-04-14 14:45:45 -0400

    Added PostOrder

--- BST.h
+++ BST.h
@@ -32,6 +32,7 @@ private:
   BSTNode<Key,E>* removehelp(BSTNode<Key, E>*, const Key&);
   E* findhelp(BSTNode<Key, E>*, const Key&) const;
   void printhelp(BSTNode<Key, E>*, int) const;
+  void printPostOrder(BSTNode<Key, E>*, int) const;
   void visit(BSTNode<Key, E>*) const;
 
 public:
@@ -196,4 +197,14 @@ printhelp(BSTNode<Key, E>* root, int level) const {
   printhelp(root->left(), level+1);   // Do left subtree
   visit(root);						  // Print node value
   printhelp(root->right(), level+1);  // Do right subtree
+}
+
+// Print out a BST postOrder
+template <typename Key, typename E>
+void BST<Key, E>::
+printPostOrder(BSTNode<Key, E>* root, int level) const {
+  if (root == NULL) return;           // Empty tree
+  printPostOrder(root->left(), level+1);   // Do left subtree
+  printPostOrder(root->right(), level+1);  // Do right subtree
+  visit(root); // Print node value
 }
